150cf71882a4ba025165b65fe8f99b0c036ca8b2
Jelani Jackson <jeljack86@gmail.com>
2021-06-09 09:08:32 -0400

    Exercise 2

---
+++ exercises/02/02.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\tI" << endl;
+    cout << "\t\t\t\"RUSH\"" << endl;
+    cout << "\t\t\t\t\t\\TO\\" << endl;
+    cout << "\t\t\tAND" << endl;
+    cout << "\t/FRO/" << endl;
+
+    return 0;
+}
